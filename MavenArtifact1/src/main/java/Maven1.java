00c5091dce24b3860c75c7f30f4ec75429cadd8d
Jyoti <Jyoti@LAPTOP-UEHCL88I>
2025-12-06 11:46:07 +0530

    add

---
+++ MavenArtifact1/src/main/java/Maven1.java
@@ -0,0 +1,17 @@
+import org.testng.annotations.Test;
+
+public class Maven1 {
+
+	public static void main(String[] args) {
+		int i=10;
+		System.out.println(i);
+
+	}
+	@Test
+	public void A() {
+		System.out.println("Maven1");
+		
+	}
+	
+
+}
